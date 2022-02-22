28ce4969bef0b187bf33a85e26b326125758def6
Randy Chase <randy.chase12@gmail.com>
2022-02-21 20:40:42 -0600

    first push

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'AI2ES HPC'
+copyright = '2022, Randy Chase'
+author = 'Randy Chase'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.0'
+version = '0.0.1'
 
 # -- General configuration
 
